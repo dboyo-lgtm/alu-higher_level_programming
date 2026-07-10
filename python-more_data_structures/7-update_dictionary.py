67e49dddbbb982d641d55a25806ebcb77154ef30
dboyo-lgtm <d.boyo@alustudent.com>
2026-07-10 09:08:13 -0700

    a function that replaces or adds key/value in a dictionary.

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a function to update a dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
